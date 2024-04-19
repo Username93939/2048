e6cdc40d79915cd3c815dd32f934339f3c1de0bf
Username93939 <128490471+Username93939@users.noreply.github.com>
2024-04-19 16:16:00 +0000

    Create index.html

---
+++ index.js
@@ -0,0 +1,17 @@
+import path, { dirname } from "path"
+import { fileURLToPath } from "url"
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
+import { createRequire } from 'module'
+const require = createRequire(import.meta.url)
+const express = require('express')
+
+const app = express()
+
+app.use(express.static('public'))
+
+app.get("/", (req, res) => {
+    res.sendFile(`${__dirname}/index.html`)
+})
+
+app.listen(3000)
